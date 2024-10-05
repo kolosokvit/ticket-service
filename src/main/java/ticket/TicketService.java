9b0d6c6d79cc6a908b753bd824cba6672658a002
Vitali Kalaskou <kolosokvit@gmail.com>
2024-10-05 23:13:41 +0300

    Add ticket storage in TicketService class

--- src/main/java/ticket/TicketService.java
+++ src/main/java/ticket/TicketService.java
@@ -1,11 +1,38 @@
 package ticket;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketService {
+    private List<Ticket> ticketList = new ArrayList<>();
+    
     public static void main(String[] args) {
-        Ticket emptyTicket = new Ticket();
-        Ticket fullTicket = new Ticket("1111", "Stadium", "111", LocalDateTime.of(2024, 11, 15, 20, 00), false, StadiumSector.A, 5.500, "500.554");
-        Ticket limitedTicket = new Ticket("Arena", "222", LocalDateTime.of(2024, 12, 31, 23, 00));
+        TicketService ticketService = new TicketService();
+        
+        final String CONCERT_HALL = "Arena";
+        final String TICKET_PRICE = "500.55";
+
+        ticketService.ticketList.add(new Ticket("0001", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0002", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0003", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0004", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0005", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0006", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0007", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0008", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0009", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+        ticketService.ticketList.add(new Ticket("0010", CONCERT_HALL, "111", LocalDateTime.of(2024, 11, 15, 20, 0),
+                false, StadiumSector.A, 5.500, TICKET_PRICE));
+
     }
 }
